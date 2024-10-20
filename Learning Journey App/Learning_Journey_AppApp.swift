4d56f100f4597e732efab35c329e02e1e2f309d5
ghadah <Ghadahx2015@gmail.com>
2024-10-20 15:33:19 +0300

    Initial Commit

---
+++ Learning Journey App/Learning_Journey_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_Journey_AppApp.swift
+//  Learning Journey App
+//
+//  Created by Ghadah Alhagbani on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Learning_Journey_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Learning Journey AppTests/Learning_Journey_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_Journey_AppTests.swift
+//  Learning Journey AppTests
+//
+//  Created by Ghadah Alhagbani on 17/04/1446 AH.
+//
+
+import Testing
+@testable import Learning_Journey_App
+
+struct Learning_Journey_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
